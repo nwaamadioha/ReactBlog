88cf4a478417938d6931a410bbc3cc417c79857e
nwaamadioha <udokwu.temple960@gmail.com>
2022-10-22 23:28:52 +0100

    Added axios for dynamic data handling and updating theCRUD operations for the API

--- Desktop/RT2/02RTUT/01rtut/src/App.js
+++ Desktop/RT2/02RTUT/01rtut/src/App.js
@@ -8,22 +8,43 @@ import PostPage from "./PostPage"
 import About from "./About"
 import Missing from "./Missing"
 
+import api from "./Api/posts"
 import { format } from 'date-fns'
-import data from "./Data"
+
 import { Routes, Route, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 
 function App() {
-  const [posts, setPosts] = useState(data)
+  const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('')
   const [searchResults, setSearchResults] = useState([])
   const [postTitle, setPostTitle] = useState('')
   const [postBody, setPostBody] = useState('')
   const navigate = useNavigate()
 
+  //Fetch posts at load time
+  useEffect (() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts')
+        setPosts(response.data)
+      } catch (err) { // Copied from the axios documentary
+        if (err.response) {
+          // Not in the 200 response range
+          console.log(err.response.data)
+          console.log(err.response.status)
+          console.log(err.response.headers)
+      } else {
+        console.log(`Error: ${err.message}`)
+      }
+     }
+    }
+    fetchPosts()
+  }, [])
+
+  //Searching through the posts
   useEffect (() => {
 
-    //Searching through the posts
     const filteredResults = posts.filter(post => 
       ((post.body).toLowerCase()).includes(search.toLowerCase())
       || ((post.title).toLowerCase()).includes(search.toLowerCase()))
@@ -31,24 +52,37 @@ function App() {
       setSearchResults(filteredResults.reverse())
   },[posts, search] )
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-   console.log(posts)
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1
     const datetime = format(new Date(), 'MMMM dd, yyyy pp')
     const newPost = {id: id, title: postTitle, datetime: datetime, body: postBody}
-    const allPosts = [...posts, newPost]
-    setPosts(allPosts)
-    console.log(allPosts)
-    setPostTitle('')
-    setPostBody('')
-    navigate("/")
+    
+    try {
+      // Send new post to the database
+      const response = await api.post('/posts', newPost)
+      // Send new post to the frontend
+      const allPosts = [...posts, response.data]
+      setPosts(allPosts)
+      setPostTitle('')
+      setPostBody('')
+      navigate("/")
+    } catch (err) {
+
+      console.log(`Error: ${err.message}`)
+    }
   }
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const postList = posts.filter(post => post.id !== id)
-    setPosts(postList)
-    navigate("/")
+    try {
+      await api.delete(`/posts/${id}`)
+      setPosts(postList)
+      navigate("/")
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
+
   }
   return (
     <div className="App">
